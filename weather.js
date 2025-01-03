0e4663e12abac1378300fc9208ae7249ec3e236c
Owen Attard <owen.attard.22@um.edu.mt>
2025-01-03 17:04:13 +0100

    Valletta openweathermap api for weather info

---
+++ weather.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=Valletta,MT&units=metric&appid=34625e58b9767aaa3559e5c385bf5ed8`;
+
+    // fetch data func
+    fetch(weatherUrl)
+        .then(response => response.json())
+        .then(data => {
+            // weather variables
+            const temperature = data.main.temp;
+            const humidity = data.main.humidity;
+            const wind = data.wind.speed;
+
+            // display format
+            document.getElementById("temperature").innerText = `Temperature: ${temperature}°C`;
+            document.getElementById("humidity").innerText = `Humidity: ${humidity}%`;
+            document.getElementById("wind").innerText = `Wind: ${wind} kph`;
+        })
+        .catch(error => {
+            console.error("Error fetching weather data:", error);
+        });
+});
